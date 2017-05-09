102153d162d1d982aabee8d66505abf50b670d82
Tawakalt <olaniyitaiwo95@yahoo.com>
2017-05-09 12:38:31 +0100

    first commit

--- app/library.js
+++ app/library.js
@@ -2,27 +2,16 @@
 
 module.exports = {
 
- /* Print all prime Numbers from 0 toN*/
- getPrimes: (n) => {
-  let x= 3,j,i=2,isPrime;
-       if (n<2) var primeArr=[];
-       else var primeArr=[2];
-       for (x; x<=n; x+=2){
-           Math.sqrt(x);
-           isPrime = true;
-           for (i = 2; i <= j; i++){
-                if (x % i === 0){
-                    isPrime = false;
-                    break;
-                }
-            }
-            if(isPrime){
-                primeArr.push(x);
-            }
-
-        }   
-
-        return primeArr;
-    }
+ findMinMax: (arraySupplied) => {    
+    arraySupplied.sort(function(a, b){return a-b});
+    let min = arraySupplied[0];
+    let max = arraySupplied[arraySupplied.length -1];
+    if (min === max)
+      return [min];
+    else
+     return [min, max];
  }
+
+}
+
 
--- tests/tests.js
+++ tests/tests.js
@@ -4,23 +4,30 @@
 
 const myApp = require('../app/library.js');
 
-describe("Arithmetic Functionality", function() {
-  it("should return [] as prime numbers between 1 and 1", function() {
-    expect(myApp.getPrimes(1)).toEqual([]);
-  })
-  it("should return [] as prime numbers between 1 and 0", function() {
-    expect(myApp.getPrimes(0)).toEqual([]);
-  })
-  it("should return [2] as prime numbers between 1 and 2", function() {
-    expect(myApp.getPrimes(2)).toEqual([2]);
-  })
-  it("should return [2,3] as prime numbers between 1 and 3", function() {
-    expect(myApp.getPrimes(3)).toEqual([2,3]);
-  })
-  it("should return [2,3,5] as prime numbers between 1 and 5", function() {
-    expect(myApp.getPrimes(5)).toEqual([2,3,5]);
-  })
-  it("should return [2,3,5,7,9] as prime numbers between 1 and 10", function() {
-    expect(myApp.getPrimes(10)).toEqual([2,3,5,7,9]);
-  })
-})
+describe('Min-Max Numbers in a List: ', function () {
+
+    describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
+
+      it('should return [1,4] for [1, 2, 3 , 4]', function () {
+        expect(myApp.findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+      });
+
+      it('should return [4, 6] for [6, 4]', function () {
+        expect(myApp.findMinMax([6, 4])).toEqual([4, 6]);
+      });
+
+      it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
+        expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+      });
+
+    });
+
+    describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
+
+      it('should return [4] for [4, 4, 4, 4]', function () {
+        expect(myApp.findMinMax([4, 4, 4, 4])).toEqual([4]);
+      });
+
+    });
+
+  });
